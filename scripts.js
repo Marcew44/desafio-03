36812bd90774410b908a9738598762d9ef050d00
Marcew44 <120995295+Marcew44@users.noreply.github.com>
2024-05-22 22:07:49 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Highlight da Navbar
+let navbar = document.querySelectorAll(".navbar a");
+let bodyId = document.querySelector("body").id;
+ 
+for(let link of navbar){
+    if(link.dataset.active == bodyId){
+        link.classList.add("active");
+    }
+}
